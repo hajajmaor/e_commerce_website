fa4ae4a46fc57ec1473095bdffdfc70624b4dcda
kono <hajajmaor@gmail.com>
2021-04-28 17:21:20 +0300

    add url regex; add product functions and schema;

---
+++ node_backend/src/config/regexs.ts
@@ -0,0 +1,2 @@
+const expression = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+const urlRegex = RegExp(expression);
---
+++ node_backend/src/models/product.ts
@@ -0,0 +1,27 @@
+import { Document } from "mongoose";
+interface IProduct extends Document {
+    title: string;
+    price: number;
+    description: string;
+    image: string;
+}
+import { Schema, model } from "mongoose";
+
+const ProductSchema: Schema = new Schema(
+    {
+        title: { type: String, required: true },
+        price: { type: Number, required: true, min: [0, 'Must be positive'] },
+        description: { type: String, required: true },
+        image: {
+            type: String,
+            required: function (this: IProduct) {
+                return this.image.match(urlRegex);
+            }
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+ProductSchema.index({ title: 1 }, { unique: true });
+export default model<IProduct>('Product', ProductSchema);
