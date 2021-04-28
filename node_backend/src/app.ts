d38ea72c7d785df05e3408d1f9b104eef763da45
kono <hajajmaor@gmail.com>
2021-04-28 15:57:57 +0300

    mongoose config

--- node_backend/src/app.ts
+++ node_backend/src/app.ts
@@ -1,16 +1,16 @@
 import express, { Application, json } from "express";
-// import mongoose from "mongoose";
-// import config from "./config/mongose-config";
+import mongoose from "mongoose";
+import config from "./config/mongose-config";
 // import bookRoutes from "./routes/book";
 import morgan from "morgan";
 
 const app: Application = express();
 const PORT = process.env.PORT || 3000;
 
-// mongoose.connect(config.url, config.options)
-//     .then(() => console.log("connected to mongodb"))
-//     .catch(err => console.log(err.message, err))
-//     ;
+mongoose.connect(config.url, config.options)
+    .then(() => console.log("connected to mongodb"))
+    .catch(err => console.log(err.message, err))
+    ;
 
 app.use(json());
 app.use(morgan('combined'))
